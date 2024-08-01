e82c7a2e03dfa3a2580f7cbb73e8826f6b470a9e
Asror <asror6857@gmail.com>
2024-08-01 10:07:18 +0500

    init: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+	let n1 = Math.abs(n)
+	let n2 = n1.toString();
+	let n3 = n2.split('').reverse().join('');
+	if(n > 0){
+		return +n3
+	}else if (n < 0){
+		return -n3
+	} else {
+		return 0
+	}
 }
