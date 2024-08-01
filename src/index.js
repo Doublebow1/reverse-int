7c4641c21b3d754f252faf2dc40fa69329aeb85b
Asror <asror6857@gmail.com>
2024-08-01 10:11:13 +0500

    fix: code

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ module.exports = function reverse (n) {
 	let n1 = Math.abs(n)
 	let n2 = n1.toString();
 	let n3 = n2.split('').reverse().join('');
-	if(n > 0){
-		return +n3
-	}else if (n < 0){
-		return -n3
-	} else {
+	if(n === 0) {
 		return 0
+	} else {
+		return n3
 	}
 }
